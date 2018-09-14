4deb60fc0b34f8e82dab972013f6c7c357cacd9c
tien <tienoho@gmail.com>
2018-09-14 16:54:29 +0700

    Add project files.

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public User GetByID(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+        public bool Login(string userName, string password)
+        {
+            var result = db.Users.Count(x => x.UserName == userName && x.Password == password);
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+using Model.Dao;
+using OnlineShop.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(LoginModel model)
+        {
+            //Kiểm tra xem form có rỗng hay không
+            if(ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.UserName, model.Password);
+                if (result)
+                {
+                    //lấy ra id của userName
+                    var user = dao.GetByID(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserID = user.ID;
+                    //gắn user vào Session
+                    Session.Add(Common.CommonConstants.USER_SESSION,userSession);
+                    //thành công thì trả về trang index home
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập không thành công!");
+                }
+            }
+            return View("Index");
+            
+        }
+    }
+}
